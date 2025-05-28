82c86f28c5b165f301e4da44c07b6bf44982bf70
yosuke <bigstart122091@gmail.com>
2025-05-28 21:44:01 +0900

    バリデーション、DB保存完了

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
 
@@ -5,5 +6,16 @@ from .forms import SignupForm
 def signup_view(request):
     print("signup")
 
-    signup_form = SignupForm()
+    # 新規登録フォームが送られてきた時
+    if request.method == 'POST':
+        print("post")
+        signup_form = SignupForm(request.POST)
+        # print(signup_form)
+        if signup_form.is_valid():
+            print("is_valid")
+            user = signup_form.save()
+            login(request, user)
+            return redirect('signup')
+    else:
+        signup_form = SignupForm()
     return render(request, 'accounts_app/signup.html', {'signup_form': signup_form})
