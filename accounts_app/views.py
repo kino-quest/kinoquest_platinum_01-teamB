8fcadbc67d8a3080210e4c6d93b21ae517112379
yosuke <bigstart122091@gmail.com>
2025-05-29 20:45:09 +0900

    ユーザー編集機能完了

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,46 +1,83 @@
-from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from .forms import LoginForm, SignupForm
+from .forms import CustomPasswordChangeForm, LoginForm, SignupForm, UserUpdateForm
 
 # 新規登録処理
 def signup_view(request):
-    print("signup")
-
     # 新規登録フォームが送られてきた時
     if request.method == 'POST':
         print("post")
         signup_form = SignupForm(request.POST)
-        # print(signup_form)
+
         if signup_form.is_valid():
             print("is_valid")
             user = signup_form.save()
             login(request, user)
-            return redirect('signup')
+            return redirect('dashboard')
     else:
         signup_form = SignupForm()
     return render(request, 'accounts_app/signup.html', {'signup_form': signup_form})
 
 # ログイン処理
 def login_view(request):
-    print("login")
-
+    # ログインフォームが送られてきた時
     if request.method == 'POST':
-        print("------------POSTで受け取ったよ-------------------")
         login_form = LoginForm(request.POST)
+
         if login_form.is_valid():
-            print("------------login_valid()-------------------")
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
-
             # 認証処理
             user = authenticate(request, username=username, password=password)
+
             if user is not None:
-                print("------------ログイン成功-------------------")
                 login(request, user)
-                return redirect('login')
+                return redirect('dashboard')
             else:
                 print("ログイン失敗")
                 login_form.add_error(None, 'メールアドレスまたはパスワードが違います')
     else:
         login_form = LoginForm()
     return render(request, 'accounts_app/login.html', {'login_form': login_form})
+
+# ログアウト処理
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# ユーザー情報設定 / 更新
+@login_required
+def user_setting(request):
+    # 「更新ボタン」が押された時
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        password_form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+
+        user_valid = user_form.is_valid()
+        password_valid = password_form.is_valid()
+
+        if user_valid:
+            user_form.save()
+        
+        if password_valid:
+            user = password_form.save()
+            #パスワード変更後にログアウトされるのを防ぐ
+            update_session_auth_hash(request, user)
+        if not user_valid and not password_valid:
+            messages.error(request, '入力内容に誤りがあります。確認してください')
+
+        # どちらも成功していたら「user_settings」へリダイレクト
+        if user_valid and password_valid:
+            messages.success(request, 'ユーザー情報を更新しました')
+            return redirect('user_setting')
+        
+    user_form = UserUpdateForm(instance=request.user)
+    password_form = CustomPasswordChangeForm(user=request.user)
+    params = {
+        'user_form': user_form,
+        'password_form': password_form,
+    }
+    return render(request, 'accounts_app/user_setting.html', params)
